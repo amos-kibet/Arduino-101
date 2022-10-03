73ca4907f51cc827a0f7da6b29bcb5ec6053f381
amos-kibet <kibetamos97@gmail.com>
2022-10-03 19:29:36 +0300

    fix: organize files & folders

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 #include <Arduino.h>
 
+// byte sensorData = 10;
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+
+    // Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);
+    // Serial.print("Sensor Data is = ");
+    // Serial.println(sensorData);
+    // sensorData = sensorData + 1;
 }
